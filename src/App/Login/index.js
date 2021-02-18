67a9ca916c548d3253c93438d6572e54dbb01d4f
Miguel Molina <miguelmolina.tk@gmail.com>
2021-02-18 22:40:26 +0100

    Added reach Router and lazy empty pages

---
+++ src/App/Login/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+// import { Link } from "@reach/router"
+
+export default () => {
+  return (
+    <>
+      <h2>Login</h2>
+    </>
+  )
+}
--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { Suspense, lazy } from "react"
+import { Router, Link } from "@reach/router"
+
+import Login from "./Login"
+const Clients = lazy(() => import("./Clients"))
+const Products = lazy(() => import("./Products"))
 
 export default () => {
-  console.log("hi");
-  return <h1>App</h1>;
-};
+  console.log("hi")
+  return (
+    <>
+      <header>
+        <Link to="/">
+          <h1>e-commerce admin</h1>
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to="clients">Clients</Link>
+            </li>
+            <li>
+              <Link to="products">Products</Link>
+            </li>
+            <li>
+              <Link to="login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <Login path="login" />
+          <Clients path="clients" />
+          <Products path="products" />
+        </Router>
+      </Suspense>
+    </>
+  )
+}
